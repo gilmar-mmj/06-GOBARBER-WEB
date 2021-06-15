6a91931d8877865465f49b9ba9035704f71b1806
gilmar-mmj <gilmar.mmj@outlook.com>
2021-06-15 19:33:37 +0100

    Creating authenticate context API

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,8 +1,11 @@
-import React, { useCallback, useRef } from 'react';
+import React, { useCallback, useRef, useContext } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
+
+import AuthContext from '../../context/AuthContext';
+import getValidationsErrors from '../../utils/getValidationsErrors';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -10,10 +13,12 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
-import getValidationsErrors from '../../utils/getValidationsErrors';
 
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
+
+  const auth = useContext(AuthContext);
+  console.log(auth);
 
   const handleSubmit = useCallback(async (data: object) => {
     try {
